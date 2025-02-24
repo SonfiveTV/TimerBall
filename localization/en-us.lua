1dc390bd1e58a6d18f698a946d32979e968b8dd8
random-facades <random.facades@gmail.com>
2025-02-24 11:59:31 -0500

    Update en-us.lua

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -4,9 +4,8 @@ return {
             c_timerball_timerball = {
                 name = "Timer Ball",
                 text = {
-                    "Create a random {C:attention}Pokemon{}",
-                    "Rarity increases with ",
-                    "enough patience!",
+                    "Create a random {V:1}#1# {C:attention}Pokemon{}",
+                    "Rarity increases in #2# rounds!",
                     "{C:inactive}(Must have room)"
                 },
             },
